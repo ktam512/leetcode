4fbbf98399c68ad77ee5291c78767019c3f4bdea
ktam512 <ktam512@gmail.com>
2023-08-24 11:05:31 +0700

    Add java code for the problem 973. K Closest Points to Origin

---
+++ Leetcode_Medium/_973. K Closest Points to Origin/Solution.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) ->
+            (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1])
+        );
+        
+        for (int[] point : points) {
+            maxHeap.offer(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        
+        int[][] result = new int[k][2];
+        int index = 0;
+        
+        while (!maxHeap.isEmpty()) {
+            result[index] = maxHeap.poll();
+            index++;
+        }
+        
+        return result;
+    }
+}
