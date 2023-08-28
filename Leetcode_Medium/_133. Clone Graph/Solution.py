fc525ef8d330855ffe440bd5af4d571bd5da2e95
ktam512 <ktam512@gmail.com>
2023-08-28 17:18:01 +0700

    Add the python solution to the 133.Clone Graph problem

---
+++ Leetcode_Medium/_133. Clone Graph/Solution.py
@@ -0,0 +1,17 @@
+
+class Solution:
+  def cloneGraph(self, node):
+    if not node:
+      return None
+    if node in self.map:
+      return self.map[node]
+
+    newNode = Node(node.val, [])
+    self.map[node] = newNode
+
+    for neighbor in node.neighbors:
+      self.map[node].neighbors.append(self.cloneGraph(neighbor))
+
+    return newNode
+
+  map = {}
