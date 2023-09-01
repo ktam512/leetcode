682e0b4888b0c494a91b76032f39523e901ea547
ktam512 <ktam512@gmail.com>
2023-09-01 09:34:35 +0700

    Add java solution to the 98. Validate Binary Search Tree problem

---
+++ Leetcode_Medium/_98. Validate Binary Search Tree/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public boolean isValidBST(TreeNode root) {
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode pred = null;
+
+    while (root != null || !stack.isEmpty()) {
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+      root = stack.pop();
+      if (pred != null && pred.val >= root.val)
+        return false;
+      pred = root;
+      root = root.right;
+    }
+
+    return true;
+  }
+}
