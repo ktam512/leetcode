73a3e66fdf768f08ffac417823d3b4b79fb890ec
ktam512 <tknguyen5@wisc.edu>
2024-05-16 17:53:52 +0700

    Second instance of 191

--- LeetcodeEasyProblems/_191. Number of 1 Bits/Solution.java
+++ LeetcodeEasyProblems/_191. Number of 1 Bits/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int hammingWeight(int n) {
         int count = 0;
         // Loop 32 times through the bit of n
-        for (int i = 0; i < 32; i++){
+        while (n != 0){
             // Check if the LSB of the current n
             // is 1 or not and count it
             if ((n & 1) == 1){
--- LeetcodeEasyProblems/_191. Number of 1 Bits/Solution.py
+++ LeetcodeEasyProblems/_191. Number of 1 Bits/Solution.py
@@ -5,12 +5,12 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for i in range(32):
+        while n:
             #Shift the inital bit number to the right
             #i times so that its LSB got changed
-            n = n >> 1
             if (n & 1) == 1:
                 count+= 1
+            n = n >> 1
         
         return count
 
